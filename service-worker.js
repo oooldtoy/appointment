684683de842753c59d43d39fc55cf6dd76aa9896
oooldtoy <51742057+oooldtoy@users.noreply.github.com>
2024-07-22 20:54:57 +0800

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,11 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open('pwa-cache').then(cache => {
       return cache.addAll([
-        '/',
-        '/index.html',
-        '/styles.css',
-        '/app.js'
+        'index.html',
+        'static/css/app.2e6bfe34408fba4194ca550c2bd769dd.css',
+        'static/js/manifest.06a1ffbb70a28d4ba8ef.js',
+        'static/js/vendor.4a1f448b533bb11cede5.js',
+        'static/js/app.ed57ec4f925421679b80.js'
       ]);
     })
   );
